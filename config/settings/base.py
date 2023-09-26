168cb25b3a93215b5b762aa510ad85f7bd3c1590
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-09-26 19:59:57 +0800

    added rants app. setup config and urls

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,6 +93,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "rantr.users",
     # Your stuff: custom apps go here
+    "rantr.rants",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("rantr.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('rants/', include('rantr.rants.urls', namespace='rants')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
