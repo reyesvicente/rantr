6818b65263b0d35bc3d8144832ee36a195d3ab73
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-10-02 10:51:30 +0800

    removed friends app

--- rantr/friends/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-class FriendsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "rantr.friends"
-    verbose_name = _("Friends")
--- rantr/friends/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from rantr.friends.views import follow_user, unfollow_user
-
-app_name = 'friends'
-urlpatterns = [
-    path('<str:username>/follow/', follow_user, name='follow'), 
-    path('<str:username>/unfollow/', unfollow_user, name='unfollow'),
-]
