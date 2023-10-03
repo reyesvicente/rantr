9682c6f2756ca75e6e03b2b806f8d1e44daf28d7
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-10-03 14:18:34 +0800

    added follows app and urls and settings

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     path('rants/', include('rantr.rants.urls', namespace='rants')),
     path('likes/', include('rantr.likes.urls', namespace='likes')),
+    path('follows/', include("rantr.follows.urls", namespace='follows')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
---
+++ rantr/follows/urls.py
@@ -0,0 +1,2 @@
+app_name = "follows"
+urlpatterns = []
