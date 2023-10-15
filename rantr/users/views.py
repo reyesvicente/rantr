4e99e47971c63fa3dbb0899b6fb168901d9e9a62
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-10-15 20:45:57 +0800

    login redirected to rants:list

--- rantr/users/views.py
+++ rantr/users/views.py
@@ -78,7 +78,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("rants:list", kwargs={"username": self.request.user.username})
 
 
 user_redirect_view = UserRedirectView.as_view()
