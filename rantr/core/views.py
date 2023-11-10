b9552f55346d381a4470e539d7149f0cce0e93f6
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-11-10 16:04:10 +0800

    added unread_list.html and updated notice.html. added also unread view

--- rantr/core/views.py
+++ rantr/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from notifications.models import Notification
+
+
+class UnreadNotificationsList(ListView):  
+    model = Notification 
+    queryset = Notification.objects.filter(unread=True)
+    template_name = 'notifications/unread_list.html'
