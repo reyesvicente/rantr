28dd0a1161fa27a05da13fd71ce12376d238ef52
Vicente Antonio G. Reyes <icenreyes@yahoo.com>
2023-11-21 13:18:42 +0800

    mark as read done

--- rantr/core/urls.py
+++ rantr/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from rantr.core.views import UnreadNotificationsList
+from rantr.core.views import UnreadNotificationsList, MarkNotificationAsReadView
 
 app_name = 'core'
 urlpatterns = [
     path('inbox/unread/', UnreadNotificationsList.as_view(), name='unread'),
+    path('notifications/mark-as-read/<int:notification_id>/', MarkNotificationAsReadView.as_view(), name='mark_as_read'),
 ]
