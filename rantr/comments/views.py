6f4fb667c4f1f495c73d1f75c791943cc8b431bc
Vicente Reyes <icenreyes@yahoo.com>
2024-11-27 16:09:39 +0800

    added AJAX for commenting in rant list and detail page

--- rantr/comments/views.py
+++ rantr/comments/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
+from django.http import JsonResponse
+from django.urls import reverse
 
 from rantr.rants.models import Rant
 from rantr.comments.models import Comment
@@ -8,9 +10,9 @@ from rantr.notifications.models import Notification
 
 
 @login_required
-def add_comment(request, slug):
+def add_comment(request, rant_uuid):
     if request.method == 'POST':
-        rant = get_object_or_404(Rant, slug=slug)
+        rant = get_object_or_404(Rant, uuid=rant_uuid)
         content = request.POST.get('content')
         parent_id = request.POST.get('parent_id')
         
@@ -29,7 +31,7 @@ def add_comment(request, slug):
                     actor=request.user,
                     verb='commented',
                     target_content_type=ContentType.objects.get_for_model(rant),
-                    target_object_id=rant.id,
+                    target_object_id=rant.uuid,
                     description=f"{request.user.username} commented on your rant"
                 )
             
@@ -45,5 +47,24 @@ def add_comment(request, slug):
                         target_object_id=comment.id,
                         description=f"{request.user.username} replied to your comment"
                     )
+            
+            # Return JSON response for AJAX requests
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({
+                    'success': True,
+                    'username': request.user.username,
+                    'content': content,
+                    'comment_id': comment.id,
+                    'redirect_url': None  # Don't redirect for AJAX requests
+                })
+            
+            # For non-AJAX requests, redirect to the referring page if available
+            referer = request.META.get('HTTP_REFERER')
+            if referer:
+                return redirect(referer)
+            
+            # Default fallback to rant detail page
+            return redirect('rants:detail', slug=rant.slug)
     
-    return redirect('rants:detail', slug=slug)
+    # If not a POST request, redirect to rant detail
+    return redirect('rants:detail', slug=rant.slug)
