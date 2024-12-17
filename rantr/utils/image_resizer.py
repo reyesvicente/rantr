d6eada72092b565526b4d79d062493b168aa268c
Vicente Reyes <icenreyes@yahoo.com>
2024-12-17 15:03:22 +0800

    rectangular image instead of square in validation

--- rantr/utils/image_resizer.py
+++ rantr/utils/image_resizer.py
@@ -4,6 +4,6 @@ from PIL import Image
 def validate_rectangular_image(image):
     image = Image.open(image)
     width, height = image.size
-    if width != height:
-        raise ValidationError("Image must be a square image.")
+    if width == height:
+        raise ValidationError("Uploaded image must be rectangular (not square).")
     return image
